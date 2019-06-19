41d7fa8b5a458b03ddea13c494035c29d2796c95
Angelo Cordon <angelocordon@gmail.com>
2019-06-19 15:51:45 -0700

    Add basic Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+import {normalize} from 'polished';
+
+import { Navbar } from './components/Navbar';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize()}
+
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    line-height: 1.5;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>StarTrak</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Navbar />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+import { rgba } from 'polished';
+
+const NavbarWrapper = styled.nav`
+  display: flex;
+  padding: 0 1rem;
+  width: 100%;
+  height: 3rem;
+  border-bottom: 1px solid ${rgba('#000', 0.3)};
+  align-items: center;
+`;
+
+export const Navbar = function () {
+  return (
+    <NavbarWrapper>
+      StarTrak
+    </NavbarWrapper>
+  )
+}
