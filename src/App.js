6cd59a251a11cdcc1c0b62d2b0f4e11b41f075b6
Angelo Cordon <angelocordon@gmail.com>
2019-06-19 17:02:43 -0700

    Add default message to authenticate with GitHub

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { normalize } from 'polished';
-
 import { Navbar } from './components/Navbar';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize()}
 
-  html {
-    font-size: 16px;
-  }
+  html { font-size: 16px }
 
-  body {
-    line-height: 1.5;
+  body { line-height: 1.5 }
+`;
+
+const MainPanel = styled.main`
+  padding: 1rem;
+`;
+
+const SignUpButton = styled.button`
+  display: block;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  background-color: #2c3e50;
+  border-color: transparent;
+  border-radius: 3px;
+  color: #fff;
+  letter-spacing: 1px;
+
+  @media (max-width: 320px) {
+    width: 100%;
   }
 `;
 
@@ -20,9 +34,18 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App">
-        <Navbar />
-      </div>
+      <Navbar />
+      <MainPanel>
+        {/* 
+          Search for repositories
+          List repositories belonging to User 
+          or List Search Results 
+        */}
+        <p>
+          Connect with your GitHub account to view your starred repositories.
+        </p>
+        <SignUpButton>Connect with GitHub</SignUpButton>
+      </MainPanel>
     </>
   );
 }
