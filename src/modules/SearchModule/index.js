96fcf63f04e2f628aa2fe29bf5f4a9d6b142a07f
Angelo Cordon <angelocordon@gmail.com>
2019-06-20 23:44:50 -0700

    Conditionally render SearchResults component
    Because GraphQL fetches a query on component mount, only conditionally render
    the component when a keyword is present in state.

--- src/modules/SearchModule/index.js
+++ src/modules/SearchModule/index.js
@@ -1,26 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import SearchField from './SearchField';
 import SearchResults from './SearchResults';
-import { Headline as ResultsHeadline } from '../../components';
-
-const renderHeadline = function(query) {
-  if (query) {
-    return <ResultsHeadline small>Search Results for {query} </ResultsHeadline>;
-  }
-
-  return;
-};
+import { LoadingUI } from '../../components';
 
 export default function SearchModule() {
   const [keyword, setKeyword] = useState();
 
   return (
-    <>
+    <section>
       <SearchField onSearch={setKeyword} />
-      <section>
-        {renderHeadline(keyword)}
-        {keyword ? <SearchResults query={keyword} /> : null}
-      </section>
-    </>
+      <Suspense fallback={<LoadingUI />}>
+        {/* NOTE: Because GraphQL will automatically fetch a query on comonent 
+            mount, only render the SearchResults component when a `keyword`
+            is present in state. Alternatively, it may also be possilbe to
+            manually fire a query using asynchronous effect hooks and the 
+            ApolloConsumer.
+            See reference: https://www.apollographql.com/docs/react/essentials/queries/#manually-firing-a-query */}
+        {keyword && <SearchResults query={keyword} />}
+      </Suspense>
+    </section>
   );
 }
