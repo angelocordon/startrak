7096ad16710c381ad0e43bf0107ab3f6f12894bd
Angelo Cordon <angelocordon@gmail.com>
2019-06-26 02:43:12 -0700

    Set up React Routes in App
    Add Switch component in return method to split between the `/search` page and
    the `/stars` page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { ApolloProvider } from 'react-apollo-hooks';
 import client from './apolloClient';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import GlobalStyle from './styles';
 import { Navbar } from './components';
 import { AuthenticateModule, AuthenticatedModule } from './modules';
+import { AuthenticatedRoute, RootRoute } from './routes';
 
 export default function App() {
   const [authenticated, setAuthenticated] = useState(true);
@@ -15,16 +17,23 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
-      <Navbar />
-      <ApolloProvider client={client}>
-        <main>
-          {!authenticated ? (
-            <AuthenticateModule handleAuthentication={authenticateUser} />
-          ) : (
-            <AuthenticatedModule />
-          )}
-        </main>
-      </ApolloProvider>
+      <Router>
+        <AuthProvider value={authContext}>
+          <Navbar />
+          <ApolloProvider client={client}>
+            <Switch>
+              <RootRoute
+                exact
+                path="/"
+                component={HomePage}
+                authComponent={StarsPage}
+              />
+              <AuthenticatedRoute path="/search" component={SearchPage} />
+              <AuthenticatedRoute path="/stars" component={StarsPage} />
+            </Switch>
+          </ApolloProvider>
+        </AuthProvider>
+      </Router>
     </>
   );
 }
