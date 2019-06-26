87d57e8f21a371a1de5d4a6a32575730f3302c44
Angelo Cordon <angelocordon@gmail.com>
2019-06-26 03:09:29 -0700

    Add SVG icon support to Button components

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
+  display: flex;
+  align-items: center;
   padding: 1rem 2rem;
   background-color: transparent;
   border-radius: 3px;
   cursor: pointer;
+
+  > svg {
+    margin-right: 0.5rem;
+  }
 `;
